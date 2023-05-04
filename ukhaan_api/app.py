0bf2312ba6c681e6590d765aceb5e0be2a43c2e4
Aashish Chapain <ashishchapain86@gmail.com>
2023-05-04 10:39:15 +0545

    feat: basic api implementation

--- ukhaan_api/app.py
+++ ukhaan_api/app.py
@@ -1,97 +1,40 @@
 from fastapi import FastAPI
-from tools import UkhaanTable, UkhaanFunctionalities, get_language_list
-
+from tools import RetrieveUkhaanAPI
 
 # Create instances of UkhaanTable and UkhaanFunctionalities classes:
-ukhaan = UkhaanTable()
-functionalities = UkhaanFunctionalities()
-
-# Get the lists of Nepali, Roman, Meaning and Example words from the UkhaanTable instance:
-nepali_lists = ukhaan.nepali()
-roman_lists = ukhaan.roman()
-meaning_lists = ukhaan.meaning()
-example_lists = ukhaan.example()
-
+ukhaan = RetrieveUkhaanAPI()
 
 # Initialize a FastAPI instance:
 app = FastAPI()
 
 
-# Define a route to get all UkhaanTukka:
+# Define a route to get all ukhaan
 @app.get("/")
-def get_ukhantukka(limit: int = 100, offset: int = 0, show_all: bool = False):
-    # Get the lists of Nepali, Roman, Meaning, and Example words based on the given parameters:
-    nepali_result = nepali(limit, offset, show_all)
-    roman_result = roman(limit, offset, show_all)
-    meaning_result = meaning(limit, offset, show_all)
-    example_result = example(limit, offset, show_all)
-
-    # Return the merged result
-    return {
-        **nepali_result,
-        **roman_result,
-        **meaning_result,
-        **example_result
-    }
+def get_ukhaan(limit: int = 100, offset: int = 0, show_all: bool = False):
+    result = ukhaan.retrieve_ukhaan(limit, offset, show_all)
+    return {**result}
 
 
 # Below defined endpoint and routes to get Nepali, Romanized version, meaning and and example usage of Ukhaan based on the given parameters:
-
 @app.get("/nepali")
 def nepali(limit: int = 100, offset: int = 0, show_all: bool = False):
-    return get_language_list("Nepali", nepali_lists, limit, offset, show_all)
+    result = ukhaan.retrieve_nepali(limit, offset, show_all)
+    return {**result}
 
 
 @app.get("/roman")
 def roman(limit: int = 100, offset: int = 0, show_all: bool = False):
-    return get_language_list("Roman", roman_lists, limit, offset, show_all)
+    result = ukhaan.retrieve_roman(limit, offset, show_all)
+    return {**result}
 
 
 @app.get("/meaning")
 def meaning(limit: int = 100, offset: int = 0, show_all: bool = False):
-    return get_language_list("Meaning", meaning_lists, limit, offset, show_all)
+    result = ukhaan.retrieve_meaning(limit, offset, show_all)
+    return {**result}
 
 
 @app.get("/example")
 def example(limit: int = 100, offset: int = 0, show_all: bool = False):
-    return get_language_list("example", example_lists, limit, offset, show_all)
-
-
-
-# Define endpoint to get a random Ukhaan.
-@app.get("/random-ukhaan")
-def random_ukhaan():    
-    return functionalities.random_ukhaan()
-
-
-
-# Below are the defined endpoints ard routes get Nepali, Romanized version, meaning and an example usage of random ukhaan.
-
-@app.get("/random-ukhaan/nepali")
-def random_nepali():
-    return {
-        'Nepali': functionalities.random_nepali().strip()
-    }
-
-
-@app.get("/random-ukhaan/roman")
-def random_roman():
-    return {
-        'Roman': functionalities.random_roman().strip()
-    }
-
-
-@app.get("/random-ukhaan/meaning")
-def random_meaning():
-    return {
-        'Meaning': functionalities.random_meaning().strip()
-    }
-
-
-@app.get("/random-ukhaan/example")
-def random_example():
-    return {
-        'Example': functionalities.random_example().strip()
-    }
-
-    
+    result = ukhaan.retrieve_example(limit, offset, show_all)
+    return {**result}
--- ukhaan_api/tools.py
+++ ukhaan_api/tools.py
@@ -1,119 +1,147 @@
-import random
+import itertools
+
 import markdown
 import requests
-import itertools
 from bs4 import BeautifulSoup
 
 
-# This little function to avoid nested lists:
-def flat_lists(d_lists):
-    return list(itertools.chain(*d_lists))
+class RetrieveUkhaan:
+    """Retrieves latest ukhaan from source and store them them pythonically"""
+
+    def __init__(self) -> None:
+        # pulling latest ukhaan
+        _request = requests.get(
+            "https://raw.githubusercontent.com/chapainaashish/nepali-ukhaan/main/README.md"
+        )
+
+        # parsing md to html
+        _html = markdown.markdown(_request.text)
+        _soup = BeautifulSoup(_html, "html.parser")
+
+        # excluding <span>, <br> elements and processing ukhaan table only
+        for span_tag in _soup.find_all(["span", "br"]):
+            span_tag.decompose()
+        _table = _soup.find_all("p")[-1]
+
+        # grouping ukhaan in nested list
+        _nested_ukhaan_lists = [tab.text.split("\n") for tab in _table]
+
+        # excluding empty string from list
+        _filtered_lists = [
+            [item for item in sublist if item != ""] for sublist in _nested_ukhaan_lists
+        ]
+
+        # flattening list/combining nested list
+        # [1:] for removing table heading
+        self._ukhaan_tables = list(itertools.chain(*_filtered_lists[1:]))
+
+    def nepali(self) -> list:
+        """
+        Retrieves a list of ukhaan in Nepali language
+        """
+        # looks for a ukhaan in nepali in first position (index 0)
+        return [raw.split("|")[0].strip() for raw in self._ukhaan_tables]
+
+    def roman(self) -> list:
+        """
+        Retrieves a list of ukhaan in Roman Nepali
+        """
+        # looks for a ukhaan in romanized nepali in second position (index 1)
+        return [raw.split("|")[1].strip() for raw in self._ukhaan_tables]
+
+    def meaning(self) -> list:
+        """
+        Retrieves a meaning for each Nepali ukhaan
+        """
+
+        # looks for a meaning in second-last and last position (index -2|-1) depending on the row length, else return empty string
+        return [
+            row.split("|")[-2].strip()
+            if len(row.split("|")) == 4
+            else row.split("|")[-1].strip()
+            if len(row.split("|")) == 3
+            else ""
+            for row in self._ukhaan_tables
+        ]
+
+    def example(self) -> list:
+        """
+        Retrieves a example for each Nepali ukhaan
+        """
+
+        # looks for a example in last position (index -1), else return empty string
+        return [
+            row.split("|")[-1].strip() if len(row.split("|")) == 4 else ""
+            for row in self._ukhaan_tables
+        ]
+
+    def ukhaan(self) -> list:
+        """
+        Retrieves a list of tuples containing ukhaan, its Romanized version, its meaning, and an example
+        """
+        return list(zip(self.nepali(), self.roman(), self.meaning(), self.example()))
 
 
-# This function retrieves a list of ukhaan and their iteration based on parameters such as limit and offset:
-def get_language_list(language_name, language_list, limit: int = 100, offset: int = 0, show_all: bool = False):
-    if show_all:
-        return {
-            language_name: language_list,
-        }
-    else:
-        return {
-            language_name: language_list[offset: offset + limit],
+class RetrieveUkhaanAPI(RetrieveUkhaan):
+    """Utility class specifically made for API call"""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _limit_ukhaan(
+        self, category_name: str, ukhaan: list, limit: int, offset: int, show_all: bool
+    ) -> dict:
+        """Retrieve ukhaan dynamically"""
+        if show_all:
+            return {
+                category_name: ukhaan,
+            }
+        else:
+            return {
+                category_name: ukhaan[offset : offset + limit],
             }
 
+    def retrieve_ukhaan(self, limit: int, offset: int, show_all: bool) -> list:
+        """
+        Retrieves a list of ukhaan in tuples
+        """
+        ukhaan = super().ukhaan()
+        return self._limit_ukhaan(
+            "Ukhaan", ukhaan, limit=limit, offset=offset, show_all=show_all
+        )
 
-# This class retrieves data from a Nepali proverb table:
-class UkhaanTable:    
-    def __init__(self): 
-        # Retrieve the raw markdown text from the `nepali-ukhaan` repository on GitHub and convert to HTML for parsing purposes.         
-        req = requests.get('https://raw.githubusercontent.com/chapainaashish/nepali-ukhaan/main/README.md')
-        html = markdown.markdown(req.text)
-        
-        # Making a soup to parse the HTML to retrieve the Nepali proverb table
-        soup = BeautifulSoup(html, 'html.parser')
+    def retrieve_nepali(self, limit: int, offset: int, show_all: bool) -> dict:
+        """
+        Retrieves a list of Nepali ukhaan in Nepali language
+        """
+        nepali_ukhaan = super().nepali()
+        return self._limit_ukhaan(
+            "Nepali", nepali_ukhaan, limit=limit, offset=offset, show_all=show_all
+        )
 
-        for span_tag in soup.find_all(['span', 'br']): span_tag.decompose()
-        self.table = soup.find_all('p')[-1]
+    def retrieve_roman(self, limit: int, offset: int, show_all: bool) -> dict:
+        """
+        Retrieves a list of Nepali ukhaan in Roman Nepali
+        """
+        roman_ukhaan = super().roman()
+        return self._limit_ukhaan(
+            "Roman", roman_ukhaan, limit=limit, offset=offset, show_all=show_all
+        )
 
-        nested_lists = [tab.text.split("\n") for tab in self.table]
-        # Filtering the list that give an empty string in a newline
-        filtered_lists = [[item for item in sublist if item != ''] for sublist in nested_lists]
-        # Flattening the nested/multi-dimensional list
-        self.ukhaan_tables = flat_lists(filtered_lists[1:])        
-    
-    # This method extracts the Nepali or Roman list from the Nepali proverb table based on the given index.
-    def extract_phase_one(self, indexes):        
-        into_list_comprehension = [tab.split("|")[indexes].strip() for tab in self.ukhaan_tables]        
-        return into_list_comprehension        
-            
-    # This method extracts the meaning and example lists from the Nepali proverb table:
-    def extract_phase_two(self):        
-        # This code snippet extracts the meaning and example lists from self.ukhaan_tables using list comprehension method.
+    def retrieve_meaning(self, limit: int, offset: int, show_all: bool) -> dict:
+        """
+        Retrieves a list of Nepali ukhaan meaning
+        """
+        ukhaan_meaning = super().meaning()
+        return self._limit_ukhaan(
+            "Meaning", ukhaan_meaning, limit=limit, offset=offset, show_all=show_all
+        )
 
-        # If a row in self.ukhaan_tables has 4 fields (i.e. separated by '|'), the second-to-last field is assumed to be the meaning
-        # and the last field is assumed to be the example. If a row has 3 fields, only the last field is assumed to be the meaning.
-        # If a row has less than 3 fields, both meaning and example are set to empty string.
-        # The resulting meaning and example lists are returned as a tuple.
-        meaning_lists = [
-                            row.split("|")[-2].strip() if len(row.split("|")) == 4 else
-                            row.split("|")[-1].strip() if len(row.split("|")) == 3 else
-                            "" for row in self.ukhaan_tables    
-                        ]
-        example_lists = [
-                            row.split("|")[-1].strip() if len(row.split("|")) == 4 else
-                            "" for row in self.ukhaan_tables        
-                        ]
-        
-        return meaning_lists, example_lists        
-        
-    # Below methods return a list of nepali, romanized version, meaning and an example usage of ukhaan.
-    
-    def nepali(self):        
-        return [nep.strip() for nep in self.extract_phase_one(0)]
-    
-    def roman(self):        
-        return [rom.strip() for rom in self.extract_phase_one(1)]  
-    
-    def meaning(self):        
-        return self.extract_phase_two()[0]
-    
-    def example(self):        
-        return self.extract_phase_two()[-1]
-
-
-# This class provides additional functionalities for the ukhaan table.
-class UkhaanFunctionalities(UkhaanTable):    
-    def __init__(self):
-        super().__init__()        
-
-    # This method returns a random ukhaan along with its corresponding romanization, meaning, and example as a dictionary.
-    def random_ukhaan(self):      
-        indexes = random.randint(0, len(self.extract_phase_one(0)))       
-
-        data = {
-                'Nepali': self.extract_phase_one(0)[indexes].strip(),
-                'Roman': self.extract_phase_one(1)[indexes].strip(),
-                'Meaning': self.extract_phase_two()[0][indexes].strip(),
-                'Example': self.extract_phase_two()[-1][indexes].strip(),
-            }
-        
-        return data
-    
-    # Below methods returns a Nepali, romanized version, meaning and an example usage of a random Nepali ukhaan.
-        
-    def random_nepali(self):        
-        indexes = random.randint(0, len(self.extract_phase_one(0)))
-        return self.extract_phase_one(0)[indexes]
-
-    def random_roman(self):    
-        indexes = random.randint(0, len(self.extract_phase_one(1)))
-        return self.extract_phase_one(1)[indexes]
-    
-    def random_meaning(self):        
-        indexes = random.randint(0, len(self.extract_phase_two()[0]))
-        return self.extract_phase_two()[0][indexes]
-    
-    def random_example(self):    
-        indexes = random.randint(0, len(self.extract_phase_two()[-1]))
-        return self.extract_phase_two()[-1][indexes]
-
+    def retrieve_example(self, limit: int, offset: int, show_all: bool) -> dict:
+        """
+        Retrieves a list of Nepali ukhaan example
+        """
+        ukhaan_example = super().example()
+        return self._limit_ukhaan(
+            "Example", ukhaan_example, limit=limit, offset=offset, show_all=show_all
+        )
